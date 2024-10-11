db4682c06e199e107655a576b25e9307661cdfde
robinmihkelson <rmihkelson06@gmail.com>
2024-10-11 08:38:13 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+<body class="">
+  <div class="flex items-center justify-center gap-4">
+    <div class="w-24 h-24 bg-red-500"></div>
+    <div class="w-24 h-24 bg-green-500"></div>
+    <div class="mt-[96px] w-48 h-48 bg-orange-500"></div>
+  </div>
+  <div class="flex items-center justify-center gap-4">
+    <div class="w-48 h-48 bg-pink-500"></div>
+    <div class="w-24 h-24 bg-blue-500"></div>
+    <div class="w-24 h-24 bg-purple-500"></div>
+  </div>
+</body>
+</template>
